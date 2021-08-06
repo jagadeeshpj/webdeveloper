2a6e3b22cf5f54dbf8626579e5545ca345cb0e3f
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-08-06 15:47:13 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,34 +1,5 @@
 
-<?php
 
-
- if(empty($_POST["name"]) ||
-    empty($_POST["email"]) ||
-    empty($_POST["subject"]) ||
-    empty($_POST["phone_number"])||
-    empty($_POST["message"]))
-    {
-        echo "Please fill the form";
-        exit;
-    }
-    
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$phone_number = $_POST["phone_number"];
-$message = $_POST["message"];
-
-mail( 'jagadeeshbose2001@gmail.com' , 'New form submission' , 
-"New form submission: Name: $name, Email:$email ,subject: $subject, phone_number: $phone_number ,message : $message");
-
-header('Location: contact.html');
-
-if(empty($_POST['submit']))
-{
-    echo "Form is not submitted!";
-    exit;
-}
-?>
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 
