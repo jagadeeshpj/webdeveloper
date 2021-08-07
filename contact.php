89b9dda3cd55749797682ee8a38acd9429068c91
JAGADEESH P <87519174+jagadeeshpj@users.noreply.github.com>
2021-08-07 19:30:59 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -177,13 +177,13 @@
 
     <script src="static/js/typewriter.js"></script>
      <!-- jQuery -->
-    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="static/js/jquery.min.js"></script>
     <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="static/js/popper.min.js"></script>
     <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="static/js/bootstrap.min.js"></script>
     <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="static/js/mdb.min.js"></script>
     <!-- Your custom scripts (optional) -->
     <script type="text/javascript">
       $(function () {
